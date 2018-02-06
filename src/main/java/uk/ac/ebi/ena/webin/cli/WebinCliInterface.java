05d900f00a2128cf0c60334473b0b529b2012dd5
Iain Cleland <cleland@ebi.ac.uk>
2018-02-06 11:25:09 +0000

    work in progress

---
+++ src/main/java/uk/ac/ebi/ena/webin/cli/WebinCliInterface.java
@@ -0,0 +1,11 @@
+package uk.ac.ebi.ena.webin.cli;
+
+import uk.ac.ebi.embl.api.validation.ValidationEngineException;
+import uk.ac.ebi.ena.manifest.ManifestObj;
+
+import java.util.List;
+
+public interface WebinCliInterface {
+	int validate() throws ValidationEngineException;
+	void setOutputDir(String outputDir);
+}
