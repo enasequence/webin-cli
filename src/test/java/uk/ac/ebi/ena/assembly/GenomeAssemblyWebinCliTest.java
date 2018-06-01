3d36d0e51f5938a2ec2843fc28ca58a70140d410
Дмитрий Смирнов <dsmirnov@ebi.ac.uk>
2018-06-01 10:54:48 +0100

    test case for ERZ403032

--- src/test/java/uk/ac/ebi/ena/assembly/GenomeAssemblyWebinCliTest.java
+++ src/test/java/uk/ac/ebi/ena/assembly/GenomeAssemblyWebinCliTest.java
@@ -210,7 +210,26 @@ public class GenomeAssemblyWebinCliTest {
         Assert.assertTrue( validator.validate() );
     }
 
+    
+    @Test public void 
+    testERZ403032() throws Exception 
+    {
+        GenomeAssemblyWebinCli validator = new GenomeAssemblyWebinCli( true );
+        URL url = GenomeAssemblyWebinCliTest.class.getClassLoader().getResource( "uk/ac/ebi/ena/assembly/genome/ERZ403032/ERZ403032.manifest" );
+        File file = new File( url.getFile() );
+        Sample sample = new Sample();
+        sample.setOrganism( "Quercus robur" );
+        
+        validator.setInputDir( file.getParentFile() );
+        validator.setValidationDir( createOutputFolder() );
+        validator.setSubmitDir( createOutputFolder() );
+        validator.defineFileTypes( file );
+        validator.setSample( sample );
+        validator.setStudy( new Study() );
+        Assert.assertTrue( validator.validate() );
+    }
 	
+    
 	private File
     createOutputFolder() throws IOException
     {
