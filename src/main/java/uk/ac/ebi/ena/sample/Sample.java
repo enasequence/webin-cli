26803ea8e4b517946d62449efac4a3c0cf0dbcd3
Дмитрий Смирнов <dsmirnov@ebi.ac.uk>
2018-11-05 17:06:01 +0000

    Fixes in source feature parser. Added unit test for source features

--- src/main/java/uk/ac/ebi/ena/sample/Sample.java
+++ src/main/java/uk/ac/ebi/ena/sample/Sample.java
@@ -19,8 +19,10 @@ import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import java.util.List;
 import java.util.stream.Collectors;
+
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+
 import org.apache.http.HttpStatus;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -33,10 +35,10 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+
 import uk.ac.ebi.embl.api.entry.feature.FeatureFactory;
 import uk.ac.ebi.embl.api.entry.feature.SourceFeature;
 import uk.ac.ebi.embl.api.entry.qualifier.Qualifier;
-import uk.ac.ebi.embl.api.validation.dao.EraproDAOUtilsImpl;
 import uk.ac.ebi.embl.api.validation.helper.MasterSourceFeatureUtils;
 import uk.ac.ebi.embl.api.validation.helper.taxon.TaxonHelperImpl;
 import uk.ac.ebi.ena.webin.cli.WebinCli;
@@ -88,99 +90,100 @@ public class Sample {
         }
     }
 
-    public static SourceFeature getSourceFeature(String sampleId, String userName, String password, boolean TEST)  {
-        try {
-        	Sample sample = new Sample();
-            String uniqueName =null;
+    
+    public static SourceFeature 
+    getSourceFeature( String sampleId, String userName, String password, boolean TEST )
+    {
+        try 
+        {
+            String uniqueName = null;
             MasterSourceFeatureUtils sourceUtils = new MasterSourceFeatureUtils();
             SourceFeature sourceFeature = new FeatureFactory().createSourceFeature();
             CloseableHttpClient httpClient = HttpClients.createDefault();
             HttpGet httpGet = new HttpGet( ( TEST ? "https://wwwdev.ebi.ac.uk/ena/submit/drop-box/samples/" : "https://www.ebi.ac.uk/ena/submit/drop-box/samples/" ) +  URLEncoder.encode( sampleId.trim(), "UTF-8" ) );
-            String encoding = Base64.getEncoder().encodeToString((userName + ":" + password).getBytes());
-            httpGet.setHeader("Authorization", "Basic " + encoding);
-            CloseableHttpResponse response = httpClient.execute(httpGet);
+            String encoding = Base64.getEncoder().encodeToString( ( userName + ":" + password ).getBytes() );
+            httpGet.setHeader( "Authorization", "Basic " + encoding );
+            CloseableHttpResponse response = httpClient.execute( httpGet );
+            
             int responsecode = response.getStatusLine().getStatusCode();
-            switch (responsecode) {
+            switch( responsecode )
+            {
                 case HttpStatus.SC_OK:
-                  //  BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent(), StandardCharsets.UTF_8));
                     DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
                     DocumentBuilder db = dbf.newDocumentBuilder();
-                    Document doc = db.parse(response.getEntity().getContent());
+                    Document doc = db.parse( response.getEntity().getContent() );
                     doc.getDocumentElement().normalize();
                     
                     // alias
-                    NodeList sampleList = doc.getElementsByTagName("SAMPLE");
-                    for (int temp = 0; temp < sampleList.getLength(); temp++) 
+                    NodeList sampleList = doc.getElementsByTagName( "SAMPLE" );
+                    for( int temp = 0; temp < sampleList.getLength(); temp++ ) 
                     {
-                    	 Node nNode = sampleList.item(temp);
+                    	 Node nNode = sampleList.item( temp );
                      
-                         if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                         if( nNode.getNodeType() == Node.ELEMENT_NODE ) 
+                         {
                             Element eElement = (Element) nNode;
-                            uniqueName=eElement.getAttribute("alias");
+                            uniqueName=eElement.getAttribute( "alias" );
                          }
                     }
+                    
                     //organism, taxid
-                     NodeList sampleNameList = doc.getElementsByTagName("SAMPLE_NAME");
-                    for (int temp = 0; temp < sampleNameList.getLength(); temp++) 
+                    NodeList sampleNameList = doc.getElementsByTagName( "SAMPLE_NAME" );
+                    for( int temp = 0; temp < sampleNameList.getLength(); temp++ ) 
                     {
-                    	 Node nNode = sampleNameList.item(temp);
-                     
-                         if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                    	 Node nNode = sampleNameList.item( temp );
+                         if( nNode.getNodeType() == Node.ELEMENT_NODE )
+                         {
                             Element eElement = (Element) nNode;
-                            sourceFeature.addQualifier(Qualifier.DB_XREF_QUALIFIER_NAME,eElement
-                                    .getElementsByTagName("TAXON_ID")
-                                    .item(0)
-                                    .getTextContent());
-                            sourceFeature.setScientificName(eElement
-                                    .getElementsByTagName("SCIENTIFIC_NAME")
-                                    .item(0)
-                                    .getTextContent());
+                            sourceFeature.addQualifier( Qualifier.DB_XREF_QUALIFIER_NAME, eElement.getElementsByTagName( "TAXON_ID" ).item( 0 ).getTextContent() );
+                         
+                            if( eElement.getElementsByTagName( "SCIENTIFIC_NAME" ).getLength() > 0 )
+                                sourceFeature.setScientificName( eElement.getElementsByTagName( "SCIENTIFIC_NAME" ).item( 0 ).getTextContent() );
                          }
                     }
                     
                     //source qualifiers
-                    NodeList attributeList = doc.getElementsByTagName("SAMPLE_ATTRIBUTE");
-                    for (int temp = 0; temp < attributeList.getLength(); temp++) 
+                   NodeList attributeList = doc.getElementsByTagName( "SAMPLE_ATTRIBUTE" );
+                   for( int temp = 0; temp < attributeList.getLength(); temp++ ) 
                    {
-                        Node nNode = attributeList.item(temp);
-                        if (nNode.getNodeType() == Node.ELEMENT_NODE) 
+                        Node nNode = attributeList.item( temp );
+                        if( nNode.getNodeType() == Node.ELEMENT_NODE ) 
                         {
                            Element eElement = (Element) nNode;
-                              String qualifierName=eElement
-                              .getElementsByTagName("TAG")
-                              .item(0)
-                              .getTextContent();
-                             String qualifierValue= eElement
-                              .getElementsByTagName("VALUE")
-                              .item(0)
-                              .getTextContent();
-                             sourceUtils.addSourceQualifier(qualifierName, qualifierValue, sourceFeature);
-
+                           String qualifierName  = eElement.getElementsByTagName( "TAG" ).item( 0 ).getTextContent();
+                           String qualifierValue = eElement.getElementsByTagName( "VALUE" ).getLength() > 0 ? eElement.getElementsByTagName( "VALUE" ).item( 0 ).getTextContent() 
+                                                                                                            : null;
+                           sourceUtils.addSourceQualifier( qualifierName, qualifierValue, sourceFeature );
                         }
                     }
                     
-                    sourceUtils.addExtraSourceQualifiers(sourceFeature,new TaxonHelperImpl(), uniqueName);
+                    sourceUtils.addExtraSourceQualifiers( sourceFeature, new TaxonHelperImpl(), uniqueName );
                    
                     return sourceFeature;
                     
                 case HttpStatus.SC_BAD_REQUEST:
                 case HttpStatus.SC_NOT_FOUND:
-                    throw WebinCliException.createValidationError(VALIDATION_ERROR_SAMPLE, sampleId);
+                    throw WebinCliException.createValidationError( VALIDATION_ERROR_SAMPLE, sampleId );
+                    
                 case HttpStatus.SC_INTERNAL_SERVER_ERROR:
-                    throw WebinCliException.createSystemError(SYSTEM_ERROR_INTERNAL);
+                    throw WebinCliException.createSystemError( SYSTEM_ERROR_INTERNAL );
+
                 case HttpStatus.SC_SERVICE_UNAVAILABLE:
-                    throw WebinCliException.createSystemError(SYSTEM_ERROR_UNAVAILABLE);
+                    throw WebinCliException.createSystemError( SYSTEM_ERROR_UNAVAILABLE );
+                
                 case HttpStatus.SC_UNAUTHORIZED:
                 case HttpStatus.SC_FORBIDDEN:
-                    throw WebinCliException.createUserError(WebinCli.AUTHENTICATION_ERROR);
+                    throw WebinCliException.createUserError( WebinCli.AUTHENTICATION_ERROR );
+                
                 default:
-                    throw WebinCliException.createSystemError(SYSTEM_ERROR_OTHER);
+                    throw WebinCliException.createSystemError( SYSTEM_ERROR_OTHER );
             }
         } catch( Exception e )
         {
-            throw WebinCliException.createSystemError(SYSTEM_ERROR_OTHER);
+            throw WebinCliException.createSystemError( SYSTEM_ERROR_OTHER );
         }
     }
+    
     public long getTaxId() {
         return taxId;
     }
