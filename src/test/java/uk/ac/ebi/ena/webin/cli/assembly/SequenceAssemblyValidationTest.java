505293a0d60005a14238730b54b779684d58138b
vijayaraja <vijayaraja@ebi.ac.uk>
2019-04-01 17:09:45 +0100

    TSV file test cases fix

--- src/test/java/uk/ac/ebi/ena/webin/cli/assembly/SequenceAssemblyValidationTest.java
+++ src/test/java/uk/ac/ebi/ena/webin/cli/assembly/SequenceAssemblyValidationTest.java
@@ -10,22 +10,9 @@
  */
 package uk.ac.ebi.ena.webin.cli.assembly;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
-import java.io.File;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.Arrays;
-import java.util.Locale;
-import java.util.Optional;
-
 import org.assertj.core.api.AssertionsForClassTypes;
 import org.junit.Before;
 import org.junit.Test;
-
 import uk.ac.ebi.embl.api.entry.genomeassembly.AssemblyInfoEntry;
 import uk.ac.ebi.embl.api.validation.Severity;
 import uk.ac.ebi.embl.api.validation.ValidationMessage;
@@ -39,6 +26,14 @@ import uk.ac.ebi.ena.webin.cli.WebinCliException;
 import uk.ac.ebi.ena.webin.cli.WebinCliMessage;
 import uk.ac.ebi.ena.webin.cli.WebinCliTestUtils;
 import uk.ac.ebi.ena.webin.cli.entity.Study;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class SequenceAssemblyValidationTest {
 
@@ -167,28 +162,6 @@ public class SequenceAssemblyValidationTest {
         return validator;
     }
 
-    private void assertTsvValidatorError(String tsvFile, String expectedReportFile, String actualReportFile) {
-        String tsvFileDir = "uk/ac/ebi/ena/webin/cli/template/";
-
-        File file = WebinCliTestUtils.getFile(tsvFileDir + tsvFile);
-        SequenceAssemblyWebinCli validator = createTsvValidator(file, FileType.TSV);
-
-        assertThatThrownBy(validator::validate).isInstanceOf(WebinCliException.class)
-                .hasMessageContaining("tsv file validation failed");
-
-        try {
-            Path expectedReportFilePath = WebinCliTestUtils.getPath(tsvFileDir + expectedReportFile);
-            Path actualReportFilePath = validator.getValidationDir().toPath().resolve(actualReportFile);
-
-            String expectedReport = new String(Files.readAllBytes(expectedReportFilePath)).replaceAll("\\s+", "");
-            String actualReport = new String(Files.readAllBytes(actualReportFilePath), StandardCharsets.UTF_8).replaceAll("\\s+", "");
-
-            assertThat(actualReport).isEqualTo(expectedReport);
-        } catch (IOException ex) {
-            throw new RuntimeException(ex);
-        }
-    }
-
     private SequenceAssemblyWebinCli createValidator(File inputDir) {
         SequenceAssemblyWebinCli cli = new SequenceAssemblyWebinCli();
         cli.setTestMode(true);
@@ -229,56 +202,6 @@ public class SequenceAssemblyValidationTest {
             validator.validate();
         }
     }
-
-    @Test
-    public void
-    testInvalidMandatoryFieldsPresent() {
-        assertTsvValidatorError(
-                "Sequence-mandatory-field-missing.tsv.gz",
-                "Sequence-mandatory-field-missing-expected-results.txt",
-                "Sequence-mandatory-field-missing.tsv.gz.report");
-    }
-
-    @Test
-    public void
-    testInvalidAlphanumericEntrynumber() {
-        assertTsvValidatorError("Sequence-invalid-alphanumeric-entrynumber-.tsv.gz",
-                "Sequence-invalid-alphanumeric-entrynumber-expected-results.txt",
-                "Sequence-invalid-alphanumeric-entrynumber-.tsv.gz.report");
-    }
-
-    @Test
-    public void
-    testInvalidMarker() {
-        assertTsvValidatorError("Sequence-invalid-marker.tsv.gz",
-                "Sequence-invalid-marker-expected-results.txt",
-                "Sequence-invalid-marker.tsv.gz.report");
-    }
-
-    @Test
-    public void
-    testInvalidSediment() {
-        assertTsvValidatorError("Sequence-invalid-sediment.tsv.gz",
-                "Sequence-invalid-sediment-expected-results.txt",
-                "Sequence-invalid-sediment.tsv.gz.report");
-    }
-
-    @Test
-    public void
-    testInvalidEntryNumberStart() {
-        assertTsvValidatorError("Sequence-invalid-entrynumber-start.tsv.gz",
-                "Sequence-invalid-entrynumber-start-expected-results.txt",
-                "Sequence-invalid-entrynumber-start.tsv.gz.report");
-    }
-
-    @Test
-    public void
-    testInvalidNonAsciiCharacters() {
-        assertTsvValidatorError("Sequence-non-ascii-characters.gz",
-                "Sequence-non-ascii-characters-expected-results.txt",
-                "Sequence-non-ascii-characters.gz.report");
-    }
-
 
     @Test
     public void
