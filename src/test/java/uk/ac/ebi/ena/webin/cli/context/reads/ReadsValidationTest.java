af809534f4e095f6daea8bead018cf3062d8023b
Eugene Ivanov <eugene@ebi.ac.uk>
2019-09-10 16:41:13 +0100

    tests refactoring

--- src/test/java/uk/ac/ebi/ena/webin/cli/context/reads/ReadsValidationTest.java
+++ src/test/java/uk/ac/ebi/ena/webin/cli/context/reads/ReadsValidationTest.java
@@ -3,12 +3,12 @@ package uk.ac.ebi.ena.webin.cli.context.reads;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static uk.ac.ebi.ena.webin.cli.WebinCliTestUtils.resourceDir;
+import static uk.ac.ebi.ena.webin.cli.context.reads.ReadsManifestReader.Field.QUALITY_SCORE;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.net.URLDecoder;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
@@ -18,10 +18,10 @@ import uk.ac.ebi.ena.webin.cli.ManifestBuilder;
 import uk.ac.ebi.ena.webin.cli.WebinCliException;
 import uk.ac.ebi.ena.webin.cli.WebinCliExecutor;
 import uk.ac.ebi.ena.webin.cli.WebinCliExecutorBuilder;
-import uk.ac.ebi.ena.webin.cli.submit.SubmissionBundle.SubmissionXMLFileType;
 import uk.ac.ebi.ena.webin.cli.validator.file.SubmissionFiles;
 import uk.ac.ebi.ena.webin.cli.validator.manifest.ReadsManifest;
 import uk.ac.ebi.ena.webin.cli.validator.manifest.ReadsManifest.FileType;
+import uk.ac.ebi.ena.webin.cli.validator.manifest.ReadsManifest.QualityScore;
 import uk.ac.ebi.ena.webin.cli.validator.response.ReadsValidationResponse;
 
 public class ReadsValidationTest {
@@ -47,100 +47,79 @@ public class ReadsValidationTest {
 
   @Test
   public void
-  manifestTwoBAMs() {
-    File manifestFile =
-        manifestBuilder()
-            .file(FileType.BAM, "file1.bam")
-            .file(FileType.BAM, "file2.bam")
-            .build();
+  fastqFileDoesNotExist() {
+    /*
+    TODO: check error messages
+    ERROR: Invalid BAM file name. Could not read file: "missing".
+    ERROR: Invalid BAM file suffix: "missing". Valid file suffixes are: .bam.
+    ERROR: Invalid CRAM file name. Could not read file: "missing".
+    ERROR: Invalid CRAM file suffix: "missing". Valid file suffixes are: .cram.
+    ERROR: Invalid FASTQ file name. Could not read file: "missing".
+    ERROR: Invalid FASTQ file suffix: "missing". Valid file suffixes are: .gz, .bz2.
+     */
 
-    assertThatThrownBy(() -> executorBuilder.readManifest(manifestFile, RESOURCE_DIR))
-        .isInstanceOf(WebinCliException.class)
-        .hasMessageStartingWith("Invalid manifest file");
+    for (FileType fileType : FileType.values()) {
+      File manifestFile =
+          manifestBuilder().file(fileType, "doesnotexisits.fastq.gz.bz2").build();
+
+      assertThatThrownBy(() -> executorBuilder.readManifest(manifestFile, RESOURCE_DIR))
+          .isInstanceOf(WebinCliException.class)
+          .hasMessageStartingWith("Invalid manifest file");
+    }
   }
 
   @Test
   public void
-  manifestTwoCRAMs() {
-    File manifestFile =
-        manifestBuilder().
-            file(FileType.CRAM, "file1.cram").
-            file(FileType.CRAM, "file2.cram").
-            build();
+  fileIsDirectory() throws IOException {
+    /*
+    TODO: check error messages
+    ERROR: Invalid BAM file name. Could not read file: "test5399168466889766446test".
+    ERROR: Invalid BAM file suffix: "test5399168466889766446test". Valid file suffixes are: .bam.
+    ERROR: Invalid CRAM file name. Could not read file: "test1251089343390577154test".
+    ERROR: Invalid CRAM file suffix: "test1251089343390577154test". Valid file suffixes are: .cram.
+    ERROR: Invalid FASTQ file name. Could not read file: "test4190692009155610561test".
+    ERROR: Invalid FASTQ file suffix: "test4190692009155610561test". Valid file suffixes are: .gz, .bz2.
+     */
 
-    assertThatThrownBy(() -> executorBuilder.readManifest(manifestFile, RESOURCE_DIR))
-        .isInstanceOf(WebinCliException.class)
-        .hasMessageStartingWith("Invalid manifest file");
-  }
+    for (FileType fileType : FileType.values()) {
+      File manifestFile =
+          manifestBuilder().file(fileType, createOutputFolder()).build();
 
-
-  @Test
-  public void
-  manifestMixingFormats() {
-    File manifestFile =
-        manifestBuilder().
-            file(FileType.BAM, "file1.bam").
-            file(FileType.CRAM, "file2.cram").
-            build();
-
-    assertThatThrownBy(() -> executorBuilder.readManifest(manifestFile, RESOURCE_DIR))
-        .isInstanceOf(WebinCliException.class)
-        .hasMessageStartingWith("Invalid manifest file");
+      assertThatThrownBy(() -> executorBuilder.readManifest(manifestFile, RESOURCE_DIR))
+          .isInstanceOf(WebinCliException.class)
+          .hasMessageStartingWith("Invalid manifest file");
+    }
   }
 
   @Test
   public void
-  manifestNoFiles() {
-    File manifestFile =
-        manifestBuilder().build();
+  fileNoPath() {
+    /*
+    TODO: check error messages
+    ERROR: No data files have been specified. Expected data files are: [1-2 FASTQ] or [1 CRAM] or [1 BAM].
+    ERROR: No data files have been specified. Expected data files are: [1-2 FASTQ] or [1 CRAM] or [1 BAM].
+    ERROR: No data files have been specified. Expected data files are: [1-2 FASTQ] or [1 CRAM] or [1 BAM].
+     */
 
-    assertThatThrownBy(() -> executorBuilder.readManifest(manifestFile, RESOURCE_DIR))
-        .isInstanceOf(WebinCliException.class)
-        .hasMessageStartingWith("Invalid manifest file");
+    for (FileType fileType : FileType.values()) {
+      File manifestFile =
+          manifestBuilder().file(fileType, "").build();
+
+      assertThatThrownBy(() -> executorBuilder.readManifest(manifestFile, RESOURCE_DIR))
+          .isInstanceOf(WebinCliException.class)
+          .hasMessageStartingWith("Invalid manifest file");
+    }
   }
 
   @Test
   public void
-  fastqFileDoesNotExist() throws IOException {
-    File manifestFile =
-        manifestBuilder().
-            file(FileType.FASTQ, "doesnotexisits.fastq.gz.bz2").
-            build();
+  fileNonASCIIPath() throws IOException {
+    /*
+    TODO: check error messages
+    ERROR: Invalid FASTQ file name. Could not read file: "FILE7663404904618886897Š.fq.gz".
+    ERROR: Invalid FASTQ file name: "FILE7663404904618886897Š.fq.gz". File name should conform following regular expression: ^([\p{Alnum}]|\\|\]|\[|#|-|_|\.|,|\/|:|@|\+| |\(|\)|'|~|<|%|\?)+$.
+     */
 
-    assertThatThrownBy(() -> executorBuilder.readManifest(manifestFile, RESOURCE_DIR))
-        .isInstanceOf(WebinCliException.class)
-        .hasMessageStartingWith("Invalid manifest file");
-  }
-
-  @Test
-  public void
-  manifestFileIsDirectory() throws IOException {
-    File manifestFile =
-        manifestBuilder().
-            file(FileType.FASTQ, createOutputFolder() + " PHRED_33").
-            build();
-
-    assertThatThrownBy(() -> executorBuilder.readManifest(manifestFile, RESOURCE_DIR))
-        .isInstanceOf(WebinCliException.class)
-        .hasMessageStartingWith("Invalid manifest file");
-  }
-
-  @Test
-  public void
-  manifestNoPath() {
-    File manifestFile =
-        manifestBuilder().
-            file(FileType.FASTQ, "").
-            build();
-
-    assertThatThrownBy(() -> executorBuilder.readManifest(manifestFile, RESOURCE_DIR))
-        .isInstanceOf(WebinCliException.class)
-        .hasMessageStartingWith("Invalid manifest file");
-  }
-
-  @Test
-  public void
-  manifestNonASCIIPath() throws IOException {
     URL url = ReadsWebinCliTest.class.getClassLoader()
         .getResource("uk/ac/ebi/ena/webin/cli/rawreads/MG23S_431.fastq.gz");
     File gz = new File(URLDecoder.decode(url.getFile(), "UTF-8"));
@@ -160,11 +139,12 @@ public class ReadsValidationTest {
 
   @Test
   public void
-  manifestFastqNoScoring() {
+  incorrectQualityScore() {
     File manifestFile =
-        manifestBuilder().
-            file(FileType.FASTQ, "file.fq.gz").
-            build();
+        manifestBuilder()
+            .file(FileType.FASTQ, "ZeroCycle_ES0_TTCCTT20NGA_0.txt.gz")
+            .field(QUALITY_SCORE, "PHRED_34")
+            .build();
 
     assertThatThrownBy(() -> executorBuilder.readManifest(manifestFile, RESOURCE_DIR))
         .isInstanceOf(WebinCliException.class)
@@ -173,73 +153,20 @@ public class ReadsValidationTest {
 
   @Test
   public void
-  manifestBAMScoring() {
+  correctQualityScore() {
     File manifestFile =
-        manifestBuilder().
-            file(FileType.BAM, "PHRED_33 file.fq.gz").
-            build();
+        manifestBuilder()
+            .file(FileType.FASTQ, "ZeroCycle_ES0_TTCCTT20NGA_0.txt.gz")
+            .field(QUALITY_SCORE, "PHRED_33")
+            .build();
 
-    assertThatThrownBy(() -> executorBuilder.readManifest(manifestFile, RESOURCE_DIR))
-        .isInstanceOf(WebinCliException.class)
-        .hasMessageStartingWith("Invalid manifest file");
+    WebinCliExecutor<ReadsManifest, ReadsValidationResponse> ex = executorBuilder.readManifest(manifestFile, RESOURCE_DIR);
+    assertThat(ex.getManifestReader().getManifest().getQualityScore()).isEqualTo(QualityScore.PHRED_33);
   }
 
   @Test
   public void
-  manifestBAMCompression() {
-    File manifestFile =
-        manifestBuilder().
-            file(FileType.BAM, "file.fq.gz").
-            build();
-
-    assertThatThrownBy(() -> executorBuilder.manifestMetadataProcessors(false).readManifest(manifestFile, RESOURCE_DIR))
-        .isInstanceOf(WebinCliException.class)
-        .hasMessageStartingWith("Invalid manifest file");
-  }
-
-  @Test
-  public void
-  manifestCRAMScoring() {
-    File manifestFile =
-        manifestBuilder().
-            file(FileType.CRAM, "PHRED_33 file.fq.gz").
-            build();
-
-    assertThatThrownBy(() -> executorBuilder.readManifest(manifestFile, RESOURCE_DIR))
-        .isInstanceOf(WebinCliException.class)
-        .hasMessageStartingWith("Invalid manifest file");
-  }
-
-  @Test
-  public void
-  manifestCRAMCompression() {
-    File manifestFile =
-        manifestBuilder().
-            file(FileType.CRAM, "file.fq.gz").
-            build();
-
-    assertThatThrownBy(() -> executorBuilder.manifestMetadataProcessors(false).readManifest(manifestFile, RESOURCE_DIR))
-        .isInstanceOf(WebinCliException.class)
-        .hasMessageStartingWith("Invalid manifest file");
-  }
-
-  @Test
-  public void
-  testCorrectBAM() {
-    File manifestFile =
-        manifestBuilder().file(FileType.BAM, "OUTO500m_MetOH_narG_OTU18.bam").build();
-
-    WebinCliExecutor<ReadsManifest, ReadsValidationResponse> executor =
-        executorBuilder.readManifest(manifestFile, RESOURCE_DIR);
-    SubmissionFiles submissionFiles = executor.getManifestReader().getManifest().files();
-    assertThat(submissionFiles.get().size()).isEqualTo(1);
-    assertThat(submissionFiles.get(FileType.BAM).size()).isOne();
-    executor.validateSubmission();
-  }
-
-  @Test
-  public void
-  testIncorrectBAM() {
+  incorrectBAM() {
     File manifestFile =
         manifestBuilder().file(FileType.BAM, "m54097_170904_165950.subreads.bam").build();
 
@@ -256,7 +183,7 @@ public class ReadsValidationTest {
 
   @Test
   public void
-  testCorrectFastq() {
+  correctFastq() {
     File manifestFile =
         manifestBuilder().file(FileType.FASTQ, "ZeroCycle_ES0_TTCCTT20NGA_0.txt.gz").build();
 
@@ -265,6 +192,20 @@ public class ReadsValidationTest {
     SubmissionFiles submissionFiles = executor.getManifestReader().getManifest().files();
     assertThat(submissionFiles.get().size()).isEqualTo(1);
     assertThat(submissionFiles.get(FileType.FASTQ).size()).isOne();
+    executor.validateSubmission();
+  }
+
+  @Test
+  public void
+  correctBAM() {
+    File manifestFile =
+        manifestBuilder().file(FileType.BAM, "OUTO500m_MetOH_narG_OTU18.bam").build();
+
+    WebinCliExecutor<ReadsManifest, ReadsValidationResponse> executor =
+        executorBuilder.readManifest(manifestFile, RESOURCE_DIR);
+    SubmissionFiles submissionFiles = executor.getManifestReader().getManifest().files();
+    assertThat(submissionFiles.get().size()).isEqualTo(1);
+    assertThat(submissionFiles.get(FileType.BAM).size()).isOne();
     executor.validateSubmission();
   }
 
@@ -290,44 +231,7 @@ public class ReadsValidationTest {
 
   @Test
   public void
-  samePairedFastq() {
-    File manifestFile =
-        manifestBuilder()
-            .file(FileType.FASTQ, "EP0_GTTCCTT_0.txt.gz")
-            .file(FileType.FASTQ, "EP0_GTTCCTT_0.txt.gz")
-            .build();
-
-    WebinCliExecutor<ReadsManifest, ReadsValidationResponse> executor =
-        executorBuilder.readManifest(manifestFile, RESOURCE_DIR);
-    SubmissionFiles submissionFiles = executor.getManifestReader().getManifest().files();
-    assertThat(submissionFiles.get().size()).isEqualTo(2);
-    assertThat(submissionFiles.get(FileType.FASTQ).size()).isEqualTo(2);
-
-    assertThatThrownBy(() -> executor.validateSubmission())
-        .isInstanceOf(WebinCliException.class)
-        .hasMessage("");
-  }
-
-  @Test
-  public void
-  pairedFastq() throws IOException {
-    File manifestFile =
-        manifestBuilder().file(FileType.FASTQ, "EP0_GTTCCTT_0.txt.gz").build();
-
-    WebinCliExecutor<ReadsManifest, ReadsValidationResponse> executor =
-        executorBuilder.readManifest(manifestFile, RESOURCE_DIR);
-    SubmissionFiles submissionFiles = executor.getManifestReader().getManifest().files();
-    assertThat(submissionFiles.get().size()).isEqualTo(1);
-    assertThat(submissionFiles.get(FileType.FASTQ).size()).isOne();
-    executor.validateSubmission();
-//    String lines = new String( Files.readAllBytes( executor.readSubmissionBundle().getXMLFileList().stream().filter(e-> SubmissionXMLFileType.EXPERIMENT.equals( e.getType() ) ).findFirst().get().getFile().toPath() ),
-//        StandardCharsets.UTF_8 );
-//    Assert.assertTrue( lines.contains( "<PAIRED" ) );
-  }
-
-  @Test
-  public void
-  fastqPair() throws IOException {
+  correctTwoFastq() {
     File manifestFile =
         manifestBuilder()
             .file(FileType.FASTQ, "EP0_GTTCCTT_S1.txt.gz")
@@ -340,14 +244,12 @@ public class ReadsValidationTest {
     assertThat(submissionFiles.get().size()).isEqualTo(2);
     assertThat(submissionFiles.get(FileType.FASTQ).size()).isEqualTo(2);
     executor.validateSubmission();
-//    String lines = new String( Files.readAllBytes( executor.readSubmissionBundle().getXMLFileList().stream().filter(e-> SubmissionXMLFileType.EXPERIMENT.equals( e.getType() ) ).findFirst().get().getFile().toPath() ),
-//        StandardCharsets.UTF_8 );
-//    Assert.assertTrue( lines.contains( "<PAIRED" ) );
+    assertThat(executor.getValidationResponse().isPaired());
   }
 
   @Test
   public void
-  fastqFalsePair() {
+  invalidTwoFastqNotPaired() {
     File manifestFile =
         manifestBuilder()
             .file(FileType.FASTQ, "EP0_GTTCCTT_P1.txt.gz")
@@ -367,7 +269,7 @@ public class ReadsValidationTest {
 
   @Test
   public void
-  testIncorrectFastq() {
+  invalidOneFastq() {
     File manifestFile =
         manifestBuilder()
             .file(FileType.FASTQ, "MG23S_431.fastq.gz")
@@ -386,7 +288,22 @@ public class ReadsValidationTest {
 
   @Test
   public void
-  testIncorrectCram() {
+  correctOneFastqPaired() {
+    File manifestFile =
+        manifestBuilder().file(FileType.FASTQ, "EP0_GTTCCTT_0.txt.gz").build();
+
+    WebinCliExecutor<ReadsManifest, ReadsValidationResponse> executor =
+        executorBuilder.readManifest(manifestFile, RESOURCE_DIR);
+    SubmissionFiles submissionFiles = executor.getManifestReader().getManifest().files();
+    assertThat(submissionFiles.get().size()).isEqualTo(1);
+    assertThat(submissionFiles.get(FileType.FASTQ).size()).isOne();
+    executor.validateSubmission();
+    assertThat(executor.getValidationResponse().isPaired());
+  }
+
+  @Test
+  public void
+  incorrectCram() {
     File manifestFile =
         manifestBuilder()
             .file(FileType.CRAM, "15194_1#135.cram")
@@ -405,7 +322,7 @@ public class ReadsValidationTest {
 
   @Test
   public void
-  testCorrectCram() {
+  correctCram() {
     File manifestFile =
         manifestBuilder().file(FileType.CRAM, "18045_1#93.cram").build();
 
@@ -416,54 +333,6 @@ public class ReadsValidationTest {
     assertThat(submissionFiles.get(FileType.CRAM).size()).isOne();
     executor.validateSubmission();
   }
-
-/*
-    @Test( timeout = 200_000 ) public void
-    openSamSmall() throws UnsupportedEncodingException
-    {
-        final SamReaderFactory factory =
-                SamReaderFactory.makeDefault().enable( SamReaderFactory.Option.VALIDATE_CRC_CHECKSUMS ).validationStringency( ValidationStringency.LENIENT );
-
-        URL url = ReadsWebinCliTest.class.getClassLoader().getResource( "uk/ac/ebi/ena/webin/cli/rawreads/16258_6#32.cram" );
-        final SamInputResource resource = SamInputResource.of( new File( URLDecoder.decode( url.getFile(), "UTF-8" ) ) );
-        ENAReferenceSource rs = new ENAReferenceSource( new String[] { } );
-        rs.setLoggerWrapper( new LoggerWrapper() {
-
-            @Override public void
-            debug( Object... messageParts )
-            {
-                System.out.println( Arrays.asList( messageParts ) );
-            }
-
-            @Override public void
-            warn( Object... messageParts )
-            {
-                System.out.println( Arrays.asList( messageParts ) );
-            }
-
-            @Override public void
-            error( Object... messageParts )
-            {
-                System.out.println( Arrays.asList( messageParts ) );
-            }
-
-            @Override public void
-            info( Object... messageParts )
-            {
-                System.out.println( Arrays.asList( messageParts ) );
-            }
-
-        } );
-        factory.referenceSource( rs );
-        final SamReader myReader = factory.open(resource);
-
-        for (final SAMRecord samRecord : myReader)
-        {
-            System.err.print(samRecord);
-        }
-
-    }
-*/
 
   private File
   createOutputFolder() throws IOException {
