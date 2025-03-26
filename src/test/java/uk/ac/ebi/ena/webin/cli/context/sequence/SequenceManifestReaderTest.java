e9a2963d1aafbefd8ca66959b5a842ffa1fc255f
dipayan1985 <dgupta@ebi.ac.uk>
2025-03-26 11:53:28 +0000

    small improvement to test

--- src/test/java/uk/ac/ebi/ena/webin/cli/context/sequence/SequenceManifestReaderTest.java
+++ src/test/java/uk/ac/ebi/ena/webin/cli/context/sequence/SequenceManifestReaderTest.java
@@ -11,11 +11,11 @@
 package uk.ac.ebi.ena.webin.cli.context.sequence;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.*;
 import static uk.ac.ebi.ena.webin.cli.context.reads.ReadsManifestReader.Field;
 
 import java.nio.file.Paths;
 import java.util.Locale;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import uk.ac.ebi.ena.webin.cli.ManifestBuilder;
@@ -25,9 +25,9 @@ import uk.ac.ebi.ena.webin.cli.WebinCliTestUtils;
 import uk.ac.ebi.ena.webin.cli.manifest.ManifestReader;
 import uk.ac.ebi.ena.webin.cli.manifest.processor.MetadataProcessorFactory;
 import uk.ac.ebi.ena.webin.cli.validator.manifest.SequenceManifest;
+import uk.ac.ebi.ena.webin.cli.validator.message.ValidationResult;
 
 public class SequenceManifestReaderTest {
-
   private static SequenceManifestReader createManifestReader() {
     WebinCliParameters parameters = WebinCliTestUtils.getTestWebinCliParameters();
     return new SequenceManifestReader(parameters, new MetadataProcessorFactory(parameters));
@@ -54,11 +54,11 @@ public class SequenceManifestReaderTest {
 
     SequenceManifest manifest = manifestReader.getManifests().stream().findFirst().get();
 
-    Assert.assertEquals("SOME-FANCY-NAME", manifest.getName());
+    assertEquals("SOME-FANCY-NAME", manifest.getName());
     assertThat(manifest.files().files()).size().isOne();
-    Assert.assertEquals("description", manifest.getDescription());
-    Assert.assertEquals("ST-001", manifest.getSubmissionTool());
-    Assert.assertEquals("STV-001", manifest.getSubmissionToolVersion());
+    assertEquals("description", manifest.getDescription());
+    assertEquals("ST-001", manifest.getSubmissionTool());
+    assertEquals("STV-001", manifest.getSubmissionToolVersion());
   }
 
   @Test
@@ -70,18 +70,21 @@ public class SequenceManifestReaderTest {
         new ManifestBuilder()
             .field(ManifestReader.Fields.NAME, " SOME-FANCY-NAME")
             .field(Field.DESCRIPTION, " description")
-            .file("FASTA", TempFileBuilder.empty("csv"))
+            .file("FASTA", TempFileBuilder.empty(".fasta.gz"))
             .field(ManifestReader.Fields.SUBMISSION_TOOL, "ST-001")
             .field(ManifestReader.Fields.SUBMISSION_TOOL_VERSION, "STV-001")
             .field("ANALYSIS_TYPE", "SEQUENCE_SET")
             .build());
+    ValidationResult validationResult = manifestReader.getValidationResult();
+
+    assertFalse(validationResult.isValid());
 
     SequenceManifest manifest = manifestReader.getManifests().stream().findFirst().get();
 
-    Assert.assertEquals("SOME-FANCY-NAME", manifest.getName());
+    assertEquals("SOME-FANCY-NAME", manifest.getName());
     assertThat(manifest.files().files()).size().isOne();
-    Assert.assertEquals("description", manifest.getDescription());
-    Assert.assertEquals("ST-001", manifest.getSubmissionTool());
-    Assert.assertEquals("STV-001", manifest.getSubmissionToolVersion());
+    assertEquals("description", manifest.getDescription());
+    assertEquals("ST-001", manifest.getSubmissionTool());
+    assertEquals("STV-001", manifest.getSubmissionToolVersion());
   }
 }
